43960c7529e61d4c92b9a59fc96408a00f55b33c
Ebrahim khasal <webtanan@gmail.com>
2026-09-05 17:54:53 +0330

    alpha: build Tiny Risk standalone and module (#1)

---
+++ src/module/index.ts
@@ -0,0 +1,1 @@
+export{moduleManifest}from'../../module.manifest';export{RiskWorkspace,type RiskWorkspaceProps}from'../components/RiskWorkspace';export{TinyRiskRepository,KEY as TINY_RISK_STORAGE_KEY}from'../domain/repository';export{clampRisk,riskScore,riskLevel,sortRisks}from'../domain/risk';export type*from'../domain/types';

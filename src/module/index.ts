b3f44280fa98d8af1f59477608a98df863aea1ae
Ebrahim khasal <webtanan@gmail.com>
2026-09-06 11:37:30 +0330

    fix(package): emit Risk module CSS

--- src/module/index.ts
+++ src/module/index.ts
@@ -1,1 +1,7 @@
-export{moduleManifest}from'../../module.manifest';export{RiskWorkspace,type RiskWorkspaceProps}from'../components/RiskWorkspace';export{TinyRiskRepository,KEY as TINY_RISK_STORAGE_KEY}from'../domain/repository';export{clampRisk,riskScore,riskLevel,sortRisks}from'../domain/risk';export type*from'../domain/types';
+import '../styles.css';
+
+export { moduleManifest } from '../../module.manifest';
+export { RiskWorkspace, type RiskWorkspaceProps } from '../components/RiskWorkspace';
+export { TinyRiskRepository, KEY as TINY_RISK_STORAGE_KEY } from '../domain/repository';
+export { clampRisk, riskScore, riskLevel, sortRisks } from '../domain/risk';
+export type * from '../domain/types';
